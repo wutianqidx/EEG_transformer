bb65ba59e044bdf1c28cc194a835318902a16221
Tianqi Wu <twu38@illinois.edu>
2020-03-09 19:29:23 -0500

    encode&position

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
 import torch
 import torch.nn as nn
+import math
 import torch.nn.functional as F
 
 
 def loss_func(output, target):
     loss = None
     return loss
+
 
 class EEGtoReport(nn.Module):
     def __init__(self):
@@ -14,9 +16,9 @@ class EEGtoReport(nn.Module):
         self.pos_encoder = PositionalEncoding()
         self.eeg_transformer = EEGTransformer()
 
-    def forward(self, input, target, len):
+    def forward(self, input, target, len, position_indicator):
         eeg_embedding = self.eeg_encoder(input)
-        eeg_embedding = self.pos_encoder(eeg_embedding, len)
+        eeg_embedding = self.pos_encoder(eeg_embedding, position_indicator)
         word_embedding = self.eeg_transformer(eeg_embedding, target, len)
         return word_embedding
 
@@ -53,36 +55,48 @@ class EEGEncoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv1d(1, 512, (18,5))
-        self.pool = nn.MaxPool1d(5, stride=3)
+        self.pool = nn.MaxPool1d(60*250-4, stride=3)
         self.batch1 = nn.BatchNorm1d(512)
 
     def forward(self, input):
-        eeg_embedding = None
+        x = self.conv1(input).squeeze()
+        x = self.pool(x).squeeze()
+        eeg_embedding = self.batch1(x)
         return eeg_embedding
 
+
 class PositionalEncoding(nn.Module):
-    def __init__(self):
-        pass
-    def forward(self, eeg_embedding, len):
-        """Return: torch.tensor((S,N,E)), where S is the sequence length, N is the batch size, E is the feature number
-        E.g. eeg_embedding, len = torch.tensor(5, 512), (1,4) -> torch.tensor(4, 2, 512) padding with 0 and then do positional encoding
-        torch.split may be helpful
-        """
-        pass
+    #def __init__(self):
+    #    pass
+    #def forward(self, eeg_embedding, len):
+    #    pass
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x, position_indicator):
+        x = x + self.pe[position_indicator, :].squeeze()
+        return self.dropout(x)
+
 
 class EEGTransformer(nn.Module):
     def __init__(self):
         super().__init__()
-        encoder_layer = nn.TransformerEncoderLayer(d_model=512, nhead=8)
-        self.transformer_encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
-        decoder_layer = nn.TransformerDecoderLayer(d_model=512, nhead=8)
-        self.transformer_decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
+        self.transformer = nn.Transformer()
 
 
     def forward(self, input, target, len):
         """E.g. input, target, len:"""
-        input = torch.randn(4, 2, 512) # (S,N,E)
+        input = torch.randn(5, 512)
         target = (torch.tensor([[0,0,1,0], [1,0,0,0], [0,1,0,0]]), torch.tensor([[0,1,0,0], [0,0,1,0]]))
-        len = (1, 4)
+        len = (2, 3)
         word_embedding = None
         return word_embedding
