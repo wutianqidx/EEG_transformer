936cf270ccbf3de9adedd4754f73fbe2c17238d4
Peiyao Li <peiyaoli2@gmail.com>
2020-03-24 01:03:12 -0500

    manually merged pre_eeg

--- preprocess.py
+++ preprocess.py
@@ -51,11 +51,11 @@ def preprocess(file_path):
         txt_f = glob(dir+"/*.txt")[0]
         report = parse_txt(txt_f, word_bag)
 
-        # edf_f_list = glob(dir+"/*.edf")
-        # eeg_raw, freq_raw = read_edf(edf_f_list)
+        edf_f_list = glob(dir+"/*.edf")
+        eeg_raw, freq_raw = read_edf(edf_f_list)
 
-        # eeg = resize_eeg(eeg_raw, freq_raw, freq)
-        eeg = None
+        eeg = resize_eeg(eeg_raw, freq_raw, freq)
+        # eeg = None
 
         data.append((eeg, report))
     return data, word_bag, freq
@@ -140,14 +140,14 @@ def parse_txt(txt_f, word_bag):
     description = description.lower()
     impression = impression.lower()
 
-    description = re.sub(r'[^\w\s]', ' <sep> ', description)
-    impression = re.sub(r'[^\w\s]', ' <sep> ', impression)
+    description = re.sub(r'[^\w\s]', ' <punc> ', description)
+    impression = re.sub(r'[^\w\s]', ' <punc> ', impression)
 
     description_token = description.split()
     impression_token = impression.split()
 
     description_token.append('<end>')
-    impression_token.append('<end>')
+    impression_token.append('<sep>')
 
     for token in description_token:
     	word_bag[token] += 1
@@ -155,21 +155,85 @@ def parse_txt(txt_f, word_bag):
     for token in impression_token:
     	word_bag[token] += 1
 
-
+    impression_token.extend(description_token)
     # print(description_token)
     # print(impression_token)
     # print(word_bag)
+    # print(len(word_bag))
     # print()
-    return [impression_token, description_token]
+    return impression_token
+
+# def read_edf(edf_f_list):
+#     """TO DO: read EEG recording and calculate value for each channel
+
+#     Arg:
+#     edf_f_list: eeg.edf file list
+#     Return:
+#     np.array(18, SampleLength), sample frequency
+
+#     16+2 channels(5 chains)
+#     channels: ('EEG FP1-REF' - 'EEG F7-REF'), ('EEG F7-REF' - 'EEG T3-REF'), ...
+#     chains: 'EEG FP1-REF', 'EEG F7-REF', 'EEG T3-REF', 'EEG T5-REF', 'EEG O1-REF'
+#             'EEG FP1-REF','EEG F3-REF', 'EEG C3-REF', 'EEG P3-REF', 'EEG O1-REF'
+#             'EEG FZ-REF', 'EEG CZ-REF', 'EEG PZ-REF'
+#             'EEG FP2-REF', 'EEG F4-REF', 'EEG C4-REF', 'EEG P4-REF', 'EEG O2-REF'
+#             'EEG FP2-REF', 'EEG F8-REF', 'EEG T4-REF', 'EEG T6-REF', 'EEG O2-REF'
+#     """
+#     eeg_raw = []
+#     for edf_f in edf_f_list:
+#         f = pyedflib.EdfReader(edf_f)
+#         # # print("birthdate patientname technician", len(f.birthdate), f.birthdate, f.patientname, f.technician)
+#         # #print("getFileDuration(self)", f.getFileDuration())
+#         # #print("samplefrequency", f.samplefrequency(0), f.samplefrequency(28))
+#         # #print("samples_in_datarecord", f.samples_in_datarecord(0), f.samples_in_datarecord(28))
+#         print("datarecords_in_file: {}s*{}  {}".format(f.datarecord_duration, f.datarecords_in_file, f.file_duration))
+#         print("startdate: {}/{}/{} {}:{}:{}".format(f.startdate_month, f.startdate_day, f.startdate_year,
+#                                                     f.starttime_hour, f.starttime_minute, f.starttime_second))
+#         print("signals_in_file", f.signals_in_file)
+#         print("getSampleFrequencies", len(f.getSampleFrequencies()), f.getSampleFrequencies())
+#         print("samples_in_file", f.samples_in_file(0), f.samples_in_file(1), f.samples_in_file(27), f.samples_in_file(28), f.samples_in_file(29))
+#         signal_labels = f.getSignalLabels()
+#         label_used = ['EEG FP1-REF', 'EEG F7-REF', 'EEG T3-REF', 'EEG T5-REF', 'EEG O1-REF',
+#                                     'EEG F3-REF', 'EEG C3-REF', 'EEG P3-REF',
+#                     'EEG FZ-REF', 'EEG CZ-REF', 'EEG PZ-REF',
+#                     'EEG FP2-REF', 'EEG F4-REF', 'EEG C4-REF', 'EEG P4-REF', 'EEG O2-REF',
+#                                 'EEG F8-REF', 'EEG T4-REF', 'EEG T6-REF']
+#         sigbufs = np.zeros((len(label_used), f.getNSamples()[0]))
+#         for i, label in enumerate(signal_labels):
+#             if label in label_used:
+#                 idx = label_used.index(label)
+#                 sigbufs[idx, :] = f.readSignal(i)
+#         print("sigbufs", sigbufs, "\n")
+#         chnbufs = np.zeros((18, f.getNSamples()[0]))
+#         # TO DO:
+
+#         eeg_raw.append(chnbufs)
+#         freq_raw = f.samplefrequency(0)
+#         f.close()
+#     eeg_raw = np.concatenate(tuple(eeg_raw), axis=1)
+#     return eeg_raw, freq_raw
+
+# def resize_eeg(eeg_raw, freq_raw, freq):
+#     """TO DO: resize eeg_raw from (18, freq_raw) per second to (18, freq) per second
+
+#     Need to find library to do linear interpolation
+
+#     Args:
+#     eeg_raw: np.array(18, SampleLength)
+#     freq_raw: frequency of sample
+#     freq: frequency we want to uniform to
+#     Return:
+#     np.array(18, SampleLengthUniformed)
+#     """
+#     eeg = eeg_raw
+#     return eeg
 
 def read_edf(edf_f_list):
     """TO DO: read EEG recording and calculate value for each channel
-
     Arg:
     edf_f_list: eeg.edf file list
     Return:
     np.array(18, SampleLength), sample frequency
-
     16+2 channels(5 chains)
     channels: ('EEG FP1-REF' - 'EEG F7-REF'), ('EEG F7-REF' - 'EEG T3-REF'), ...
     chains: 'EEG FP1-REF', 'EEG F7-REF', 'EEG T3-REF', 'EEG T5-REF', 'EEG O1-REF'
@@ -179,7 +243,10 @@ def read_edf(edf_f_list):
             'EEG FP2-REF', 'EEG F8-REF', 'EEG T4-REF', 'EEG T6-REF', 'EEG O2-REF'
     """
     eeg_raw = []
+    freq_raw = 0
     for edf_f in edf_f_list:
+        # flag for valid 18 channels existance
+        valid_flag = True
         f = pyedflib.EdfReader(edf_f)
         # # print("birthdate patientname technician", len(f.birthdate), f.birthdate, f.patientname, f.technician)
         # #print("getFileDuration(self)", f.getFileDuration())
@@ -197,26 +264,44 @@ def read_edf(edf_f_list):
                     'EEG FZ-REF', 'EEG CZ-REF', 'EEG PZ-REF',
                     'EEG FP2-REF', 'EEG F4-REF', 'EEG C4-REF', 'EEG P4-REF', 'EEG O2-REF',
                                 'EEG F8-REF', 'EEG T4-REF', 'EEG T6-REF']
-        sigbufs = np.zeros((len(label_used), f.getNSamples()[0]))
-        for i, label in enumerate(signal_labels):
+        channel_tup = [('FP1','F7'),('F7','T3'),('T3','T5'),('T5','O1'),
+                       ('FP1','F3'),('F3','C3'),('C3','P3'),('P3','O1'),
+                       ('FZ','CZ'),('CZ','PZ'),('FP2','F4'),('F4','C4'),
+                       ('C4','P4'),('P4','O2'),('FP2','F8'),('F8','T4'),
+                       ('T4','T6'),('T6','O2')]
+        sigbufs = {}
+        print("signal_labels",signal_labels)
+        for i,label in enumerate(signal_labels):
             if label in label_used:
-                idx = label_used.index(label)
-                sigbufs[idx, :] = f.readSignal(i)
-        print("sigbufs", sigbufs, "\n")
+                sigbufs[label] = f.readSignal(i)
+
+        for key in sigbufs:
+            print(len(sigbufs[key]))
         chnbufs = np.zeros((18, f.getNSamples()[0]))
         # TO DO:
-
-        eeg_raw.append(chnbufs)
+        for idx,(first,second) in enumerate(channel_tup):
+            try:
+                chnbufs[idx] = sigbufs['EEG '+ first +'-REF'] - sigbufs['EEG '+ second +'-REF']
+            except:
+                print("in "+ edf_f +" pair EEG "+ first +'-REF, EEG '+ second +'-REF not found')
+                valid_flag = False
+                break
+        if not valid_flag:
+            f.close()
+            continue
+        if len(eeg_raw) == 0:
+            eeg_raw = chnbufs
+        else:
+            eeg_raw = np.concatenate((eeg_raw,chnbufs),axis=1)
+        if freq_raw != 0 and freq_raw != f.samplefrequency(0):
+            raise Exception("inconsistent frequency between "+ freq_raw + "and " +f.samplefrequency(0) )
         freq_raw = f.samplefrequency(0)
         f.close()
-    eeg_raw = np.concatenate(tuple(eeg_raw), axis=1)
     return eeg_raw, freq_raw
 
 def resize_eeg(eeg_raw, freq_raw, freq):
     """TO DO: resize eeg_raw from (18, freq_raw) per second to (18, freq) per second
-
     Need to find library to do linear interpolation
-
     Args:
     eeg_raw: np.array(18, SampleLength)
     freq_raw: frequency of sample
@@ -224,8 +309,17 @@ def resize_eeg(eeg_raw, freq_raw, freq):
     Return:
     np.array(18, SampleLengthUniformed)
     """
-    eeg = eeg_raw
-    return eeg
+    #total second of eeg_raw, need all file have same frequency
+    seconds = eeg_raw.shape[1]/freq_raw
+    time_stamp_prev = np.arange(0,seconds,1./freq_raw)
+    time_stamp_new = np.arange(0,seconds,1./freq)
+    eeg = []
+    for channel in eeg_raw:
+        new_channel = []
+        for time_item in time_stamp_new:
+            new_channel.append(np.interp(time_item,time_stamp_prev,channel))
+        eeg.append(new_channel)
+    return np.array(eeg)
 
 if __name__ == '__main__':
     file_path = "dataset/"
